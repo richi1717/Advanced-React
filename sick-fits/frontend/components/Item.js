462541327acaab80e801e8e55eb21c6fface56ae
Richi Jeffery <rici1717@gmail.com>
2020-01-19 01:10:17 -0700

    add styling to fix items in list

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -5,33 +5,39 @@ import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
-import DeleteItem from "./DeleteItem";
+import DeleteItem from './DeleteItem';
 
-const Item = ({ item }) => {
-  return (
-    <ItemStyles>
-      {item.image && <img src={item.image} alt={item.title} />}
-      <Title>
-        <Link href={{ pathname: '/item', query: { id: item.id } }}>
-          <a>{item.title}</a>
-        </Link>
-      </Title>
-      <PriceTag>{formatMoney(item.price)}</PriceTag>
-      <p>{item.description}</p>
-      <div className="butotnList">
-        <Link
-          href={{
-            pathname: 'update',
-            query: { id: item.id }
-          }}
-        >
-          <a>Edit ✏️</a>
-        </Link>
-        <DeleteItem id={item.id}>Delete This Item</DeleteItem>
-      </div>
-    </ItemStyles>
-  );
-};
+const Item = ({ item }) => (
+  <ItemStyles>
+    {item.image && <img src={item.image} alt={item.title} />}
+
+    <Title>
+      <Link
+        href={{
+          pathname: '/item',
+          query: { id: item.id }
+        }}
+      >
+        <a>{item.title}</a>
+      </Link>
+    </Title>
+    <PriceTag>{formatMoney(item.price)}</PriceTag>
+    <p>{item.description}</p>
+
+    <div className="buttonList">
+      <Link
+        href={{
+          pathname: 'update',
+          query: { id: item.id }
+        }}
+      >
+        <a>Edit ✏️</a>
+      </Link>
+      <button>Add to Cart</button>
+      <DeleteItem id={item.id}>Delete This Item</DeleteItem>
+    </div>
+  </ItemStyles>
+);
 
 Item.propTypes = {
   item: PropTypes.object.isRequired
